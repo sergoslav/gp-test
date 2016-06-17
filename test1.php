316565592af31a76d263ee25a7710f7f26b7ca37
sergoslav <sergoslav@gmail.com>
2016-06-18 01:34:50 +0300

    up

--- test1.php
+++ test1.php
@@ -5,25 +5,36 @@ require 'DynamicsGP/autoload.php';
 
 
 $options = array(
-    'login'         => $configurations['login'],
-    'password'      => $configurations['password'],
+//    'login'         => $configurations['login'],
+//    'password'      => $configurations['password'],
 //    'wsdl'          => $configurations['url'],
     'location'      => $configurations['url'],
     'soap_version'  => SOAP_1_2,
     'trace'         => 1,
     'exceptions'    => 1,
-    'connection_timeout'    => 180,
+    'connection_timeout'    => 240,
     'encoding'      => 'UTF-8',
     'cache_wsdl'    => WSDL_CACHE_NONE,
 
-    'proxy_host'     => "24.52.105.174",
-    'proxy_login'    => $configurations['login'],
-    'proxy_password' => $configurations['password']
+//    'proxy_host'     => "24.52.105.174",
+//    'proxy_login'    => $configurations['login'],
+//    'proxy_password' => $configurations['password']
 );
 
 $wsdl = $configurations['url'];
 
+
+$nameSpace = 'DynamicsGP';
+$headerData = array(
+    'UsernameToken' => array(
+        'Username' => $configurations['login'],
+        'Password' =>  $configurations['password']
+    )
+);
+$header = new SOAPHeader($nameSpace, 'Security', $headerData, true);
+
 $service = new \DynamicsGP($options, $wsdl);
+$service->__setSoapHeaders($header);
 
 $context = new \Context();
 

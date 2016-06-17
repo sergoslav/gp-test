227eed7e7231ccdd500f20fd0c2ff577a17cb8b3
sergoslav <sergoslav@gmail.com>
2016-06-17 20:47:33 +0300

    up

--- test1.php
+++ test1.php
@@ -8,12 +8,17 @@ $options = array(
     'login'         => $configurations['login'],
     'password'      => $configurations['password'],
 //    'wsdl'          => $configurations['url'],
+    'location'      => $configurations['url'],
     'soap_version'  => SOAP_1_2,
     'trace'         => 1,
     'exceptions'    => 1,
     'connection_timeout'    => 180,
     'encoding'      => 'UTF-8',
     'cache_wsdl'    => WSDL_CACHE_NONE,
+
+//    'proxy_host'     => "24.52.105.174",
+//    'proxy_login'    => $configurations['login'],
+//    'proxy_password' => $configurations['password']
 );
 
 $wsdl = $configurations['url'];
@@ -26,4 +31,13 @@ $companyKey = new \CompanyKey();
 $companyKey->setId(2);
 
 $getCompanyKeys = new \GetCompanyByKey($companyKey, $context);
-$service->GetCompanyByKey($getCompanyKeys);
+
+try{
+    $service->GetCompanyByKey($getCompanyKeys);
+} catch (Exception $e) {
+
+    echo "go exseption\n";
+    var_dump($e->getMessage());
+    print_r($e->getTrace());
+}
+
